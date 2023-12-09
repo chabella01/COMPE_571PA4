55558760faee6e97edc4d7f560940a89bcb8fd76
gsmith0123 <102399410+gsmith0123@users.noreply.github.com>
2023-12-08 20:17:50 -0800

    Update Steven_PA4_FIFO.py

--- Steven_PA4_FIFO.py
+++ Steven_PA4_FIFO.py
@@ -3,6 +3,7 @@ class PageTableEntry:
         self.physical_page_number = None
         self.dirty = False
         self.referenced = False
+        self.accessed_time = None  # Track last access time
 
 class ProcessMemory:
     def __init__(self):
@@ -11,22 +12,22 @@ class ProcessMemory:
 class PhysicalMemory:
     def __init__(self, size):
         self.size = size
-        self.pages = {}  # empty to map virtual page to physical page
+        self.pages = {}  # Map virtual page to physical page
         self.queue = []  # For FIFO page replacement
 
     def load_page(self, virtual_page, process_number):
-        # Evict a page if memory is full
         if len(self.pages) >= self.size:
             self.evict_page()
-
-        physical_page = len(self.pages)  # Assign next available physical page number
+        physical_page = len(self.pages)
         self.pages[(process_number, virtual_page)] = physical_page
         self.queue.append((process_number, virtual_page))
         return physical_page
 
     def evict_page(self):
-        # Evict the oldest page (FIFO)
         evicted_process, evicted_page = self.queue.pop(0)
+        evicted_entry = processes[evicted_process].page_table[evicted_page]
+        if evicted_entry.dirty:
+            stats.dirty_page_writes += 1
         del self.pages[(evicted_process, evicted_page)]
 
 class Statistics:
@@ -43,42 +44,29 @@ def get_offset(address):
 
 def process_memory_references(file_path, physical_memory, stats):
     processes = {}
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                parts = line.split()
+                process_number = int(parts[0])
+                address = int(parts[1])
+                operation = parts[2]
+                if process_number not in processes:
+                    processes[process_number] = ProcessMemory()
+                page_number = get_virtual_page_number(address)
+                offset = get_offset(address)
+                page_table_entry = processes[process_number].page_table[page_number]
+                if page_table_entry.physical_page_number is None:
+                    stats.page_faults += 1
+                    stats.disk_references += 1
+                    page_table_entry.physical_page_number = physical_memory.load_page(page_number, process_number)
+                if operation == 'W':
+                    page_table_entry.dirty = True
+                print(f"Process {process_number}, Address: {address}, Page: {page_number}, Offset: {offset}, Operation: {operation}")
+    except IOError:
+        print("Error reading file")
 
-    with open(file_path, 'r') as file:
-        for line in file:
-            parts = line.split()
-            process_number = int(parts[0])
-            address = int(parts[1])
-            operation = parts[2]
-
-            if process_number not in processes:
-                processes[process_number] = ProcessMemory()
-
-            page_number = get_virtual_page_number(address)
-            offset = get_offset(address)
-
-            page_table_entry = processes[process_number].page_table[page_number]
-
-            if page_table_entry.physical_page_number is None:
-                # Page fault occurs
-                stats.page_faults += 1
-                stats.disk_references += 1
-                page_table_entry.physical_page_number = physical_memory.load_page(page_number, process_number)
-
-            if operation == 'W':
-                page_table_entry.dirty = True
-
-            # Check if evicted page is dirty
-            evicted_process, evicted_page = physical_memory.queue[0] if physical_memory.queue else (None, None)
-            evicted_page_table_entry = processes[evicted_process].page_table[evicted_page] if evicted_process is not None else None
-            if evicted_page_table_entry and evicted_page_table_entry.dirty:
-                stats.dirty_page_writes += 1
-                stats.disk_references += 1
-                evicted_page_table_entry.dirty = False  # Reset dirty bit after writing back
-
-            print(f"Process {process_number}, Address: {address}, Page: {page_number}, Offset: {offset}, Operation: {operation}")
-
-# initialize physical memory and statistics
+# Initialize physical memory and statistics
 physical_memory = PhysicalMemory(32)
 stats = Statistics()
 
