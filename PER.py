fb6b23d3b297105786fc0c9a6528f63a97bac222
gsmith0123 <102399410+gsmith0123@users.noreply.github.com>
2023-12-08 22:54:35 -0800

    Update PER.py

--- PER.py
+++ PER.py
@@ -1,45 +1,34 @@
-import random
-
 class PageTableEntry:
     def __init__(self):
         self.physical_page_number = None
         self.dirty = False
         self.referenced = False
+        self.accessed_time = None  # Track last access time
 
 class ProcessMemory:
     def __init__(self):
-        self.page_table = [PageTableEntry() for _ in range(128)]
+        self.page_table = [PageTableEntry() for _ in range(128)]  # 128 entries
 
 class PhysicalMemory:
     def __init__(self, size):
         self.size = size
-        self.pages = {}
+        self.pages = {}  # Map virtual page to physical page
+        self.queue = []  # For FIFO page replacement
 
     def load_page(self, virtual_page, process_number):
         if len(self.pages) >= self.size:
             self.evict_page()
         physical_page = len(self.pages)
         self.pages[(process_number, virtual_page)] = physical_page
+        self.queue.append((process_number, virtual_page))
         return physical_page
 
     def evict_page(self):
-        categories = [[], [], [], [], []]
-        for (process_number, virtual_page), _ in self.pages.items():
-            entry = processes[process_number].page_table[virtual_page]
-            if entry.physical_page_number is None:
-                continue
-            category_index = 2 * entry.referenced + entry.dirty
-            categories[category_index].append((process_number, virtual_page))
-
-        for category in categories:
-            if category:
-                evicted_page = min(category, key=lambda x: x[1])
-                del self.pages[evicted_page]
-                break
-
-    def reset_reference_bits(self):
-        for (process_number, virtual_page), _ in self.pages.items():
-            processes[process_number].page_table[virtual_page].referenced = False
+        evicted_process, evicted_page = self.queue.pop(0)
+        evicted_entry = processes[evicted_process].page_table[evicted_page]
+        if evicted_entry.dirty:
+            stats.dirty_page_writes += 1
+        del self.pages[(evicted_process, evicted_page)]
 
 class Statistics:
     def __init__(self):
@@ -54,7 +43,6 @@ def get_offset(address):
     return address & 0x1FF
 
 def process_memory_references(file_path, physical_memory, stats):
-    reference_counter = 0
     processes = {}
     try:
         with open(file_path, 'r') as file:
@@ -63,37 +51,29 @@ def process_memory_references(file_path, physical_memory, stats):
                 process_number = int(parts[0])
                 address = int(parts[1])
                 operation = parts[2]
-
                 if process_number not in processes:
                     processes[process_number] = ProcessMemory()
-
                 page_number = get_virtual_page_number(address)
+                offset = get_offset(address)
                 page_table_entry = processes[process_number].page_table[page_number]
-
                 if page_table_entry.physical_page_number is None:
                     stats.page_faults += 1
                     stats.disk_references += 1
                     page_table_entry.physical_page_number = physical_memory.load_page(page_number, process_number)
-
                 if operation == 'W':
                     page_table_entry.dirty = True
-
-                page_table_entry.referenced = True
-
-                reference_counter += 1
-                if reference_counter >= 200:
-                    physical_memory.reset_reference_bits()
-                    reference_counter = 0
-
-                print(f"Process {process_number}, Address: {address}, Page: {page_number}, Operation: {operation}")
-
+                print(f"Process {process_number}, Address: {address}, Page: {page_number}, Offset: {offset}, Operation: {operation}")
     except IOError:
         print("Error reading file")
 
+# Initialize physical memory and statistics
 physical_memory = PhysicalMemory(32)
 stats = Statistics()
+
+# Process the memory references
 process_memory_references('data1.txt', physical_memory, stats)
 
+# Print statistics
 print(f"Total Page Faults: {stats.page_faults}")
 print(f"Total Disk References: {stats.disk_references}")
 print(f"Total Dirty Page Writes: {stats.dirty_page_writes}")
